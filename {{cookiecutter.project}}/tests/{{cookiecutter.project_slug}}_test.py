8c399f81dfbb61903c7090fde7ea3acbe5a1d133
julian diez <julian.diez.contractor@bbva.com>
2019-07-23 15:17:11 +0200

    Add different frameworks for tests

--- {{cookiecutter.project}}/tests/{{cookiecutter.project_slug}}_test.py
+++ {{cookiecutter.project}}/tests/{{cookiecutter.project_slug}}_test.py
@@ -1,6 +1,16 @@
+{% if cookiecutter.test_suite == "unittest" %}
 import unittest
 
 class TestTrue(unittest.TestCase):
 
     def test_true(self):
         self.assertTrue(True)
+{% elif cookiecutter.test_suite == "pytest" %}
+import pytest
+
+class TestTrue():
+
+    def test_true(self):
+        assert True
+{% endif %}
+
